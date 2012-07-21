36995b94511c95f8997476045b79a4c4f890a9df
adamjacobbecker <ad@mbecker.cc>
2012-07-21 14:13:08 -0700

    add station

--- app/map.js
+++ app/map.js
@@ -31,6 +31,9 @@ $(function () {
   function addPoints(data) {
 
     $.each(data, function(_, station) {
+      if(!station.latitude || station.latitude == ""){
+        return;
+      }
       var markerLocation = new L.LatLng(station.latitude, station.longitude)
       var marker = new L.Marker(markerLocation)
       map.addLayer(marker);
@@ -43,19 +46,14 @@ $(function () {
 
   }
 
-  /*$.ajax({
-    url: 'http://www.govhub.org/',
+  $.ajax({
+    url: 'http://oaklandpumpsitup.dev/stations.json',
     dataType: 'json',
     success: function (data) {
-
+      console.log(data)
       addPoints(data);
-
     }
-  });*/
-
-  data = [{"address":"6124 Telegraph Ave. Oakland, CA","bearing":45.0,"created_at":"2012-07-21T19:50:29Z","description":"Hours are 11-5 Tuesday-Friday, 11-6 Saturday-Sunday.","distance":0.0,"id":2,"latitude":37.847803,"longitude":-122.2606935,"name":"The Spoke Cyclery","station_type":"Bike Shop","updated_at":"2012-07-21T19:50:29Z"},{"address":"2424 Webster St Oakland, CA","bearing":225.0,"created_at":"2012-07-21T19:50:31Z","description":"Mon-Fri 10 am - 7 pm. Sat 10 am - 6 pm. Sun 12 pm - 5 pm","distance":1.8249525722405944,"id":4,"latitude":37.8138747,"longitude":-122.2646485,"name":"Bay Area Bikes","station_type":"Bike Shop","updated_at":"2012-07-21T19:50:31Z"},{"address":"300 Lakeshore Oakland, CA","bearing":135.0,"created_at":"2012-07-21T19:50:30Z","description":"Pump chained to park bench at corner of lakeshore and 20th.","distance":2.518519432718994,"id":3,"latitude":37.79984839999999,"longitude":-122.2565425,"name":"Lakeshore Repair Station","station_type":"Citizen Sponsored","updated_at":"2012-07-21T19:50:30Z"}]
-  addPoints(data)
-
+  });
   
 
 
